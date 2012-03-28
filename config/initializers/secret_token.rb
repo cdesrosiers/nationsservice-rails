27b421d6f349a3e55a1996bba85e51d74b05cf29
Corey Desrosiers <cdesrosiers@hp6700.home>
2012-03-27 23:02:50 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nationsservice::Application.config.secret_token = '07ad58dc2ff75d8b07190afd20b1cf1507322d7189ffc90fea5742002aa3594117de562f572b80e14260b1b9b392e8e36ae9dea7d8483a673560fe59dea347bc'

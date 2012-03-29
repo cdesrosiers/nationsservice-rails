979c69b65525c75ceff9e9f45b53e9bfd697a12b
Corey Desrosiers <cmdesrosiers@gmail.com>
2012-03-29 00:01:39 -0400

    Finished general site layout

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,4 +7,7 @@ class StaticPagesController < ApplicationController
 
   def contact
   end
+  
+  def home
+  end
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,16 +1,32 @@
 require 'spec_helper'
 
 describe "Static pages" do
+  
+  subject { page }
+  
+  describe "Home page" do
+    
+    before { visit root_path }
+  
+    it { page.should have_selector('h1', text: "Nations' Service") }
+  
+    it "should have the base title" do
+      page.should have_selector('title',
+                        text: "Nations' Service")
+    end
+  end
+
+
 
   describe "About page" do
+    
+    before { visit about_path }
 
     it "should have the content 'About Us'" do
-      visit '/static_pages/about'
       page.should have_content('About Us')
     end
     
     it "should have the right title" do
-      visit '/static_pages/about'
       page.should have_selector('title', :text => "Nations' Service | About Us")
     end
 
@@ -18,27 +34,27 @@ describe "Static pages" do
   end
   
   describe "Contact page" do
-
+    
+    before { visit contact_path }
+ 
     it "should have the content 'Contact'" do
-      visit '/static_pages/contact'
       page.should have_content('Contact')
     end
     
     it "should have the right title" do
-      visit '/static_pages/contact'
       page.should have_selector('title', :text => "Nations' Service | Contact")
     end
   end
   
   describe "Help page" do
-
+    
+    before { visit help_path }
+    
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
       page.should have_content('Help')
     end
     
     it "should have the right title" do
-      visit '/static_pages/help'
       page.should have_selector('title', :text => "Nations' Service | Help")
     end
   end

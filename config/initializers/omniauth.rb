096ebf1a1c4185fe938085d3f477dc5cc3e44a31
cdesrosiers <cmdesrosiers@gmail.com>
2012-06-15 14:29:10 -0400

    handled failed twitter authentication case on oauth callback

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,12 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, '0h7yxYQJhPEyMPygHFXtw', 'pwF7CbSGlbf644mgcwgKJcKunjU62cJaFU9m0Y9oc'
+  
+  class SafeFailureEndpoint < OmniAuth::FailureEndpoint
+    def call
+      redirect_to_failure
+    end
+  end
+
+  OmniAuth.config.on_failure = SafeFailureEndpoint
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Nationsservice::Application.routes.draw do
   devise_for :users, controllers: { registrations: 'registrations' }
 
   match 'auth/:provider/callback', to: 'authentications#create'
+  match 'auth/failure', to: 'authentications#index'
   resources :authentications, only: [ :create, :destroy, :index ]
   resources :positions
 
